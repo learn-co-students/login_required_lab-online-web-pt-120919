83c67c250d5b2717aee6af8ba4310bddb1f4291e
Justin <justinms@outlook.com>
2020-05-31 11:09:55 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name] == ""
+      redirect_to root_path
+    else
+      session[:name] = params[:name]
+      redirect_to welcome_path
+    end
+  end
+
+  def welcome
+  end
+
+  def destroy
+    session[:name] = nil
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'sessions#new'
+  get '/welcome', to: 'sessions#welcome'
+  get '/destroy', to: 'sessions#destroy'
+  resources :sessions, only: [:create]
+
+  get '/secret/show', to: 'secrets#show'
 end
